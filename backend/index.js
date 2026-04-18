9d98652af315ed858a94820e281b9f687fec4ec0
Vitaliy Yashin <vtljashin@gmail.com>
2026-04-18 15:28:32 +0300

    Firs commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 5000
+app.use(express.json())
+app.use('/', (req, res)=>{
+  res.json("Hello from Effective Mobile!")
+})
+
+app.listen(PORT, ()=>{
+  console.log(`The Server started on a ${PORT} port`)
+})
